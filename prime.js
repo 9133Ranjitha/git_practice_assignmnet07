d1b84550f28857c5fdfc83b7ec4aa44b08bd1b4a
S.Ranjitha <112804191+9133Ranjitha@users.noreply.github.com>
2022-11-02 18:24:06 +0530

    its created

---
+++ prime.js
@@ -0,0 +1,25 @@
+function checkprime(num){
+     
+     let count=0;
+
+    for( let i=0;i<=num;i++){
+     
+     if(num%i==0){
+         count++;
+      
+}
+      if(count==2){
+          return true;
+      }else{
+          return false;
+      
+}
+}
+
+
+let ans = checkprime(13);
+
+if(ans == true){
+   console.lo("prime");
+  }else{
+     console.log("not prime");
